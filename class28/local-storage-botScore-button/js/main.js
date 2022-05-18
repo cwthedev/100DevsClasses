830bb00ac09d18d60c066e11b82cebcbc6b0b37a
cw100dev <toadix@msn.com>
2022-05-18 12:48:51 -0700

    fixed local storage botscore

--- class28/local-storage-botScore-button/js/main.js
+++ class28/local-storage-botScore-button/js/main.js
@@ -2,12 +2,15 @@
 if(!localStorage.getItem('botScore')){
     localStorage.setItem('botScore', 0)
 }
-
+document.querySelector('h2').innerText = localStorage.getItem('botScore')
 document.querySelector('button').addEventListener('click', anothaOne)
 
 function anothaOne(){
     let botScore = Number(localStorage.getItem('botScore'))
     botScore = botScore + 1
     localStorage.setItem('botScore', botScore)
+    document.querySelector('h2').innerText = localStorage.getItem('botScore')
+
+
 }
 

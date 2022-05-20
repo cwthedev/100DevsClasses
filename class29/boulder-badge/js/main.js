ebcc10fd7add4a7426e4cddad609768064f7fbf3
cw100dev <toadix@msn.com>
2022-05-19 23:18:15 -0700

    class 29 boulder and cascade complete

--- class29/boulder-badge/js/main.js
+++ class29/boulder-badge/js/main.js
@@ -1,8 +1,24 @@
 // Variables
 //You are getting ready to face Brock for the Boulder Badge. You want three pokemon on your team that each have evolved at least once. You have bulbasaur which is at level 5 and evolves at level 16, caterpie which is at level 1 and evolves at level 7, and weedle which is at level 1 and evolves at level 7. Create three variables to store the number of rare candies each of your pokemon would need to evolve (rare candies increase your level by one). Finally, create a fourth variable named totalCandies that sums all the rare candies you would need. 
+let bulbasaurRC = 16-5;
+let caterpieRC = 7-1;
+let weedleRC = 7-1;
+let totalCandies = bulbasaurRC + caterpieRC + weedleRC;
 
 //Conditionals And Functions
 //You have a charmander in your party. Charmander can only battle if the temperature is above 0 degrees celcius. Create one function that converts a Fahrenheit value to Celcius and another fuction that tells you wheither or not charmander can battle
+function convertFtoC(deg){
+    return (deg-32)*5/9;
+}
+function canCharmanderBattle(degInF){
+    return (convertFtoC(degInF) > 0)? 'YES': 'NO'
+}
 
 //Loops
 //You have joined an undeground pokemon leauge. In this league, trainers can use any number of pokemon. Print to the console "Pikachu I choose you" x times where x is the number of pokemon the trainer you are battling has in their party
+
+let numPokemon = 5;
+
+for (let i=0; i < numPokemon; i++){
+    console.log('Pikachu I choose you');
+}
--- class29/cascade-badge/js/main.js
+++ class29/cascade-badge/js/main.js
@@ -1,11 +1,36 @@
 //Arrays
 //Your pokemon party order which is a list of pokemon has been leaked to Misty. Please create a function that reverses your list and prints it to the console. 
+let myPokemon = ['Pikachu', 'Snorlax', 'Bulbasaur'];
+function foolMisty(arr){
+    console.log(arr.reverse())
+}
+
+foolMisty(myPokemon);
 
 //Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
+let a = [5,7,20,25]
+let b = [100,1,1]
+function compareSumAndCube(arr1, arr2){
+    return (arr1.reduce((prev,current) => prev += current**2, 0 ) > arr2.reduce((prev,current) => prev += current**2, 0))
+}
+console.log( compareSumAndCube(a,b))
 
 //Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
 // Some cases:
 // [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
 // [68, -1, 1, -7, 10, 10] => [-1, 10]
 
+function getIndexMultiples(arr){
+    return arr.filter((x,i) => (x%i == 0))
+}
+
+console.log(getIndexMultiples([22, -6, 32, 82, 9, 25]))
+console.log(getIndexMultiples([68, -1, 1, -7, 10, 10]))
+
 //Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.Return your answer as a number.
+
+function sumRegardless(arr1){
+    return arr1.reduce((prev,cur)=> prev + +cur,0)
+}
+ 
+console.log(sumRegardless([22, -6, 32, 82, 9, 25, 68, -1, 1, -7, 10, 10]))

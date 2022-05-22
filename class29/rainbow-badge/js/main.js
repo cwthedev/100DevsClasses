2d5fc5f24ebd929bc4ca5a7fa7c6f4729ecdec2f
cw100dev <toadix@msn.com>
2022-05-22 11:24:39 -0700

    class 29: added 1/2 damage and 0 damage tables with comparisons

--- class29/rainbow-badge/js/main.js
+++ class29/rainbow-badge/js/main.js
@@ -15,13 +15,44 @@ ground: ['fire','electric','poison', 'rock', 'steel'],
 flying: ['grass', 'fighting','bug'],
 psychic: ['fighting','poison'],
 bug: ['grass','psychic','dark'],
-rock: ['firef','ice','flying','bug'],
+rock: ['fire','ice','flying','bug'],
 ghost: ['psychic','ghost'],
 dragon: ['dragon'],
 dark: ['psychic','ghost'],
 steel: ['ice','rock','fairy'],
 fairy: ['fighting','dragon','dark']
 };
+const weakChart = {
+  normal: ['rock','steel'],
+  fire: ['fire','water','rock','dragon'],
+  water: ['water','grass','dragon'],
+  electric: ['electric','grass','dragon'],
+  grass: ['fire','grass','poison','flying','bug','dragon','steel'],
+  ice: ['fire','water','ice','steel'],
+  fighting: ['poison','flying','psychic','bug','fairy'],
+  poison: ['poison','ground','rock','ghost'],
+  ground: ['grass','bug'],
+  flying: ['electric','rock','steel'],
+  psychic: ['psychic','steel'],
+  bug: ['fire','fighting','poison','flying','ghost','steel','fairy'],
+  rock: ['fighting','ground','steel'],
+  ghost: ['dragon'],
+  dragon: ['steel'],
+  dark: ['dark','fairy'],
+  steel: ['fire','water','electric','steel'],
+  fairy: ['fire','poison','steel']
+  };
+
+  const nulChart = {
+    normal: ['ghost'],
+    electric: ['ground'],
+    fighting: ['ghost'],
+    poison: ['steel'],
+    ground: ['flying'],
+    psychic: ['dark'],
+    ghost: ['normal'],
+    dragon: ['fairy']
+  }
 
 function getFetch(){
   const poke1 = document.querySelector('#poke1').value
@@ -46,9 +77,15 @@ function getFetch(){
           pokeImg.push(data.sprites.front_shiny)
           document.querySelector('#pokeImg1').src = pokeImg[0]
           document.querySelector('#pokeImg2').src = pokeImg[1]
-          console.log(pokeStore[1])
-          if(strChart[pokeStore[0]].includes(pokeStore[1])){
+          console.log(`${pokeStore[0]} vs ${pokeStore[1]}`)
+          if(Object.keys(nulChart).includes(pokeStore[0]) && nulChart[pokeStore[0]].includes(pokeStore[1])){
+            document.querySelector('h2').innerText = " 0 "
+          }
+          else if(strChart[pokeStore[0]].includes(pokeStore[1])){
             document.querySelector('h2').innerText = " 2x > "
+          }
+          else if(weakChart[pokeStore[0]].includes(pokeStore[1])){
+            document.querySelector('h2').innerText = " 1/2x > "
           }
           else{
             document.querySelector('h2').innerText = " = "

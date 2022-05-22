632eafd740dd0260577a85f8c9e90eaac52f125f
cw100dev <toadix@msn.com>
2022-05-21 20:47:11 -0700

    class29: finished the strong against comparisons. Anything not strong against shows as equal.

--- class29/rainbow-badge/js/main.js
+++ class29/rainbow-badge/js/main.js
@@ -2,6 +2,27 @@
 //original only shows data for graxx vs water type?
 document.querySelector('button').addEventListener('click', getFetch)
 
+const strChart = {
+normal: [],
+fire: ['grass','ice','bug','steel'],
+water: ['fire','ground','rock'],
+electric: ['water','flying'],
+grass: ['water','ground','rock'],
+ice: ['grass','ground','flying','dragon'],
+fighting: ['normal','ice','rock','dark','steel'],
+poison: ['grass', 'fairy'],
+ground: ['fire','electric','poison', 'rock', 'steel'],
+flying: ['grass', 'fighting','bug'],
+psychic: ['fighting','poison'],
+bug: ['grass','psychic','dark'],
+rock: ['firef','ice','flying','bug'],
+ghost: ['psychic','ghost'],
+dragon: ['dragon'],
+dark: ['psychic','ghost'],
+steel: ['ice','rock','fairy'],
+fairy: ['fighting','dragon','dark']
+};
+
 function getFetch(){
   const poke1 = document.querySelector('#poke1').value
   const poke2 = document.querySelector('#poke2').value
@@ -23,11 +44,14 @@ function getFetch(){
 
           pokeStore.push(data.types[0].type.name)
           pokeImg.push(data.sprites.front_shiny)
-      
-          if((pokeStore[0] === "grass" && pokeStore[1] === 'water')){
-            document.querySelector('#pokeImg1').src = pokeImg[0]
-            document.querySelector('#pokeImg2').src = pokeImg[1]
+          document.querySelector('#pokeImg1').src = pokeImg[0]
+          document.querySelector('#pokeImg2').src = pokeImg[1]
+          console.log(pokeStore[1])
+          if(strChart[pokeStore[0]].includes(pokeStore[1])){
             document.querySelector('h2').innerText = " 2x > "
+          }
+          else{
+            document.querySelector('h2').innerText = " = "
           }
         })
         .catch(err => {
